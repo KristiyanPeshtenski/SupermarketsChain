bdbe6babbff6ebf45202f5a0029f3c6a04e6994a
iliankostov <ilian.kostov@gmail.com>
2015-07-17 14:33:15 +0300

    Add OracleSource.sql and separate InitialDatabase.sql to MSSQLSchemaOnly.sql and MSSQLMockingData.sql

--- Sources/MSSQLMockingData.sql
+++ Sources/MSSQLMockingData.sql
@@ -1,44 +1,5 @@
-﻿USE master
-GO
-
-CREATE DATABASE SupermarketsChain
-GO
-
 USE SupermarketsChain
 GO
-
-CREATE TABLE Vendors (
-	Id INT PRIMARY KEY IDENTITY,
-	Name NVARCHAR(50) NOT NULL
-)
-
-CREATE TABLE Measures (
-	Id INT PRIMARY KEY IDENTITY,
-	Name NVARCHAR(50) NOT NULL
-)
-
-CREATE TABLE Products (
-	Id INT PRIMARY KEY IDENTITY,
-	VendorId INT NOT NULL FOREIGN KEY REFERENCES Vendors(Id),
-	Name NVARCHAR(50) NOT NULL,
-	MeasureId INT NOT NULL FOREIGN KEY REFERENCES Measures(Id),
-	Price MONEY NOT NULL
-)
-
-CREATE TABLE Supermarkets (
-	Id INT PRIMARY KEY IDENTITY,
-	Name NVARCHAR(50) NOT NULL
-)
-
-CREATE TABLE SupermarketsProducts (
-	SupermarketId INT FOREIGN KEY REFERENCES Supermarkets(Id),
-	ProductId INT FOREIGN KEY REFERENCES Products(Id)
-	CONSTRAINT [PK_SupermarketsProducts] PRIMARY KEY CLUSTERED 
-	(
-		SupermarketId ASC,
-		ProductId ASC
-	)
-)
 
 INSERT INTO Vendors (Name) 
 Values 
@@ -88,7 +49,7 @@ Values
 (9, 'Madzharov yellow cheese', 1, 16.00),
 (10, 'Milka kids chockolate', 1, 2.00),
 (11, 'Shushard dark chocolate', 1, 1.00),
-(12, 'Аlmonds', 1, 5.50),
+(12, 'Almonds', 1, 5.50),
 (12, 'Hazelnuts', 1, 4.50),
 (12, 'Cashew', 1, 5.00),
 (13, 'Zagorka beer', 1, 1.00),
---
+++ Sources/MSSQLSchemaOnly.sql
@@ -0,0 +1,68 @@
+﻿USE master
+GO
+
+CREATE DATABASE SupermarketsChain
+GO
+
+USE SupermarketsChain
+GO
+
+CREATE TABLE Vendors (
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Measures (
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Products (
+	Id INT PRIMARY KEY IDENTITY,
+	VendorId INT NOT NULL FOREIGN KEY REFERENCES Vendors(Id),
+	Name NVARCHAR(50) NOT NULL,
+	MeasureId INT NOT NULL FOREIGN KEY REFERENCES Measures(Id),
+	Price MONEY NOT NULL
+)
+
+CREATE TABLE Supermarkets (
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE SupermarketsProducts (
+	SupermarketId INT FOREIGN KEY REFERENCES Supermarkets(Id),
+	ProductId INT FOREIGN KEY REFERENCES Products(Id)
+	CONSTRAINT [PK_SupermarketsProducts] PRIMARY KEY CLUSTERED 
+	(
+		SupermarketId ASC,
+		ProductId ASC
+	)
+)
+
+CREATE TABLE VendorExpensesByMonth(
+	Id INT PRIMARY KEY NOT NULL,
+	VendorId INT FOREIGN KEY REFERENCES Vendors(Id) NOT NULL,
+	[Month] NVARCHAR(50) NOT NULL,
+	Expenses money
+)
+GO
+
+/* Delete data and reset identity
+
+DELETE SupermarketsProducts
+DELETE Supermarkets
+DBCC CHECKIDENT ('Supermarkets', RESEED,  0);
+
+ALTER TABLE SupermarketsProducts NOCHECK CONSTRAINT ALL
+DELETE Products
+DBCC CHECKIDENT ('Products', RESEED,  0);
+ALTER TABLE SupermarketsProducts CHECK CONSTRAINT ALL
+
+DELETE Measures
+DBCC CHECKIDENT ('Measures', RESEED,  0);
+
+DELETE Vendors
+DBCC CHECKIDENT ('Vendors', RESEED,  0);
+
+*/
