a0593a4b4dd4f30fa9383b7cdd1d33cfe0388810
unknown <emilia.pavlova@gmail.com>
2015-07-22 02:59:50 +0300

    Import xls data from zip files into SQL

---
+++ SupermarketsChain/ZipIntoSql/ZIPArchiveToSQLServer.cs
@@ -0,0 +1,34 @@
+﻿namespace SupermarketsChain
+{
+    class ZIPArchiveToSQLServer
+    {
+        static void Main()
+        {
+            var db = new SupermarketsChainEntities();
+            //string filePath = @"C:\Users\Emily\Documents\SoftUni\DatabaseApplications\Teamwork\Repo\Sources\Sales-Reports.zip";
+            string filePath = @"..\..\..\..\Sources\Sales-Reports.zip";
+            ZipFileReader.ReadZipFile(filePath);
+            var context = new SupermarketsChainEntities();
+
+            //var productName = Console.ReadLine(); // domati
+            //var product = context.Products.FirstOrDefault(p => p.Name == productName); // PRoduct
+
+            //if (product == null)
+            //{
+            //    product = new Product() {Name = productName};
+            //    context.Products.Add(product);
+            //    context.SaveChanges();
+            //}
+
+            //var novZapis = new Sale()
+            //{
+            //    OrderedOn = DateTime.Now,
+            //    ProductId = product.Id,
+            //};
+
+            //context.Sales.Add(novZapis);
+            //context.SaveChanges();
+
+        }
+    }
+}
---
+++ SupermarketsChain/ZipIntoSql/ZipFileReader.cs
@@ -0,0 +1,47 @@
+﻿namespace SupermarketsChain
+{
+    using System;
+    using System.IO;
+    using System.Text.RegularExpressions;
+    using Ionic.Zip;
+
+    public class ZipFileReader
+    {
+        public static void ReadZipFile(string filePath)
+        {
+            using (ZipFile zip = ZipFile.Read(filePath))
+            {
+                foreach (var file in zip)
+                {
+                    if (file.FileName.Contains(".xls"))
+                    {
+                        string pathInsideZip = file.FileName;
+                        Console.WriteLine("Title " + pathInsideZip);
+                        Regex regex = new Regex(@"((?:[0-9]+){0,2}-[a-zA-Z]+-(?:[0-9]+){4})\.xls");
+                        var dateOrder = regex.Match(pathInsideZip).Groups[1].Value;
+                        Console.WriteLine(dateOrder);
+                        Console.WriteLine();
+                        string fullPath = filePath.Replace("Sales-Reports.zip", "Sales") + '\\' + pathInsideZip.Replace('/', '\\');
+                        file.Extract("C:../../../../Sources/Sales/", ExtractExistingFileAction.OverwriteSilently);
+                        XlsFileReader.DateOrder = dateOrder;
+                        XlsFileReader.ReadXls(fullPath);
+                    }
+                }
+            }
+        }
+
+        public static void ReadDirectory(string filePath)
+        {
+            string[] subDirectories = Directory.GetDirectories(filePath);
+            foreach (string subDir in subDirectories)
+            {
+                string[] xlsFiles = Directory.GetFiles(subDir);
+
+                foreach (string xlsFile in xlsFiles)
+                {
+                    XlsFileReader.ReadXls(xlsFile);
+                }
+            }
+        }
+    }
+}
