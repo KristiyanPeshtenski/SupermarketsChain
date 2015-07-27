f206de0c29bc24d92efc7527a2fb6b75833d62c1
KristiyanPeshtenski <kristiyanpeshtenski@gmail.com>
2015-07-27 16:14:41 +0300

    update XML import and add interface.

--- SupermarketsChain/ExpenseXMLReportToSQLServer/XmlReportParser.cs
+++ SupermarketsChain/ExpenseXMLReportToSQLServer/XmlReportParser.cs
@@ -1,69 +1,63 @@
-﻿namespace SupermarketsChain
+﻿namespace SupermarketsChain.ExpenseXmlReportToSQLServer
 {
     using System;
     using System.Linq;
     using System.Xml.Linq;
     using System.Xml.XPath;
+    using System.IO;
 
-    public class XmlReportParser
+    public class XmlReportParser : IXmlParser
     {
-        public void ReadXmlFile(string path)
+        private const string BasePath = "..\\..\\..\\..\\Sources\\";
+
+        public XmlReportParser(SupermarketsChainEntities db)
         {
-            var context = new SupermarketsChainEntities();
-            var vendors = context.Vendors.Select(e => e);
-            // Load Xml file.
-            var xmlReport = XDocument.Load(@path);
-            var vendorNodes = xmlReport.XPathSelectElements("expenses-by-month/vendor");
-
-            foreach (var vendorNode in vendorNodes)
-            {
-                var vendorName = vendorNode.Attribute("name").Value;
-                var vendor = vendors.FirstOrDefault(v => v.Name == vendorName);
-                var expensesNodes = vendorNode.Elements("expenses");
-
-                if (vendor != null)
-                {
-                    foreach (var expensesNode in expensesNodes)
-                    {
-                        var month = DateTime.Parse(expensesNode.Attribute("month").Value);
-                        decimal expense = decimal.Parse(expensesNode.Value);
-                        var expence = new Expens
-                        {
-                            Month = month,
-                            Expense = expense
-                        };
-                        expence.Vendors.Add(vendor);
-                        context.Expenses.Add(expence);
-                        context.SaveChanges();
-                    }
-                }
-            }
-
-            Console.WriteLine("Report successfully imported");
+            this.Db = db;
         }
 
-        // <Summary>
-        //      Validate data from xml file and check for null vlaues.
-        // </Summary>
-        // TODO: have to decide what happen when vendor expence by month are duplicate. 
+        public SupermarketsChainEntities Db { get; set; }
 
-        //public VendorExpensesByMonth ValidateXmlData(Vendor vendor, string month, decimal? expence)
-        //{
-        //    if (vendor == null)
-        //    {
-        //        throw new ArgumentException("Invalid vendor name.");
-        //    }
-        //    if (String.IsNullOrEmpty(month))
-        //    {
-        //        throw new ArgumentException("month cannot be empty");
-        //    }
 
-        //    if (expence == null)
-        //    {
-        //        throw new ArgumentException("Month expence cannot be null.");
-        //    }
+        public void ImportXmlExpenses(string file)
+        {
+            var path = BasePath + file;
+            var vendors = this.Db.Vendors.Select(v => v);
 
-        //    return CreateExpenceByMonth(vendor, month, expence);
-        //}
+            if (File.Exists(path))
+            {
+                var xmlReport = XDocument.Load(path);
+                var vendorNodes = xmlReport.XPathSelectElements("expenses-by-month/vendor");
+                foreach (var vendorNode in vendorNodes)
+                {
+                    var vendorName = vendorNode.Attribute("name").Value;
+                    var vendor = vendors.FirstOrDefault(v => v.Name == vendorName);
+                    var expensesNodes = vendorNode.Elements("expenses");
+
+                    if (vendor != null)
+                    {
+                        foreach (var expensesNode in expensesNodes)
+                        {
+                            var month = DateTime.Parse(expensesNode.Attribute("month").Value);
+                            decimal expense = decimal.Parse(expensesNode.Value);
+                            var expence = new Expens
+                            {
+                                Month = month,
+                                Expense = expense
+                            };
+
+                            expence.Vendors.Add(vendor);
+                            this.Db.Expenses.Add(expence);
+                            this.Db.SaveChanges();
+                        }
+                    }
+                }
+
+                Console.WriteLine("Report successfully imported");
+            }
+            else
+            {
+                throw new ArgumentException("File do not exist.");
+            }
+        }
     }
 }
